6bf237a7bea9c9803572c8a0c7ef02962f8b9b06
hsminchala <83968950+hsminchala@users.noreply.github.com>
2022-02-03 19:08:01 -0500

    Pagina de inicio y usuario mejoradas

--- gestionFinanzas/src/app/inicio/inicio.component.ts
+++ gestionFinanzas/src/app/inicio/inicio.component.ts
@@ -5,10 +5,8 @@ import { Component, OnInit } from '@angular/core';
   templateUrl: './inicio.component.html',
   styleUrls: ['./inicio.component.scss'],
 })
-export class InicioComponent implements OnInit {
-
-  constructor() { }
-
-  ngOnInit() {}
+export class InicioComponent {
+  constructor() {
+  }
 
 }
